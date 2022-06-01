278fe89bce9ddd13f732ecbb24e7cd3ae8d31b2e
Sebastian Valdivia <sebastianvaldivia@gmail.com>
2022-05-31 21:44:32 -0700

    Updating Landing Page social icons

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,23 +4,23 @@ import Icon from './Icon';
 
 const socialNetworkData = [
   {
-    url: 'https://www.facebook.com/gnarfunkelband/',
+    url: 'https://www.facebook.com/mylesfromhome',
     name: 'facebook',
   },
   {
-    url: 'https://www.instagram.com/gnarfunkelband/',
+    url: 'https://www.instagram.com/mylesfromhome/',
     name: 'instagram',
   },
   {
-    url: 'https://open.spotify.com/artist/2mwAEuKKSnlOQouag2TUe5?si=GvAb4WlaQd60MaN1HJvzyQ',
+    url: 'https://open.spotify.com/artist/4aHc82sUCkKkkn8dnaMktt',
     name: 'spotify',
   },
   {
-    url: 'https://www.tiktok.com/@gnarfunkelband?',
+    url: 'https://www.tiktok.com/@mylesfromhome',
     name: 'tiktok',
   },
   {
-    url: 'https://youtube.com/channel/UCgLC96DuNoKz23AX2bGchvw',
+    url: 'https://www.youtube.com/channel/UCBz9N6vLJhpLLzmhhTFX5Ow/playlists',
     name: 'youtube',
   },
 ];
