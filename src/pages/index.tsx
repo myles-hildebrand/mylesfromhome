81109274b9d1f32e9b6bf373a922881713bd7ec8
myles-hildebrand <45373211+myles-hildebrand@users.noreply.github.com>
2025-03-24 22:46:31 -0700

    Update index.tsx
    added You're Going to Go Far live video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,9 @@ import SEO from '../components/seo';
 import MainSection from '../components/MainSection';
 import InstagramFeed from '../components/InstagramFeed';
 
-const musicVideoIds = ['lJKyf1b6nN8', //Myles from Home - Life in the Sun (Official Music Video)
-                       'hkszcg3Alc8']; //Myles From Home - LIVE at Blue Light Sessions
+const musicVideoIds = ['K-CUs8lI8f8', //You’re Gonna Go Far - Live at Fox Cabaret
+                       'lJKyf1b6nN8']; //Myles from Home - Life in the Sun (Official Music Video)
+                       //'hkszcg3Alc8', //Myles From Home - LIVE at Blue Light Sessions
                        //'YdGEDSZ2WM4', //Myles from Home - Life in the Sun (Live in Vancouver)  
                        //'vsT4zibcbCg', //Myles from Home - Don't Get on the Train (Lyric Video)
                        //'xOLALNeZaIQ'];//Myles from Home - Nearest Star (Lyric Video)
